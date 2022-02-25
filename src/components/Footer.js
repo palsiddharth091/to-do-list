d7e8a0b9dd92ecc00ce77872f688f51a4264e9f9
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2022-02-25 23:08:28 +0530

    Changed Footer to component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>   <footer>Made with 🧠 by <a href="loading.html">Siddharth Pal</a> </footer></>
+
+
+    )
+}
+
+export default Footer
